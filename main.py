d55e49671fa5a2927ed735007402c1a1eb98d091
Angelina <mathswin1@gmail.com>
2024-09-21 14:23:26 +0400

    python

--- main.py
+++ main.py
@@ -26,17 +26,6 @@ def calculate_y(n0, h, nk, a, b, c):
         results.append(y)
     return results
 
-
-def fun(n0, h, nk, a, b, c):
-    results = []
-    print(n0, h, nk, a, b, c)
-    for x in range(n0, nk, h):
-        y = (math.sin(a * x + b)) ** 2 + (math.cos(c * x)) ** 2  # Example function
-        print(y)
-        results.append(y)
-    print(results)
-
-
 def write_results_to_csv(results, file_path):
     with open(file_path, mode='w', newline='') as file:
         writer = csv.writer(file)
@@ -53,7 +42,6 @@ def main():
     config = read_config(config_file)
 
     results = calculate_y(config['n0'], config['h'], config['nk'], config['a'], config['b'], config['c'])
-    # fun(config['n0'], config['h'], config['nk'], config['a'], config['b'], config['c'])
     write_results_to_csv(results, 'results')
 
 main()
